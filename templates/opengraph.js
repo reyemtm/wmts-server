cdc8688cb78607685a034645470e6fc32066a454
Meyer <malcolmtmeyer@gmail.com>
2021-12-14 07:21:21 -0500

    favicon

--- templates/opengraph.js
+++ templates/opengraph.js
@@ -20,6 +20,12 @@ module.exports = (opts) => {
   ${(config.TWITTER) ? `<meta property="twitter:card" content="summary_large_image" />
   <meta property="twitter:image" content="${(config.SITE_IMG) ? config.SITE_IMG : `https://${config.HOST}/preview.png`}" />
   <meta property="twitter:site" content="${config.TWITTER}" />` : ''}
+  <!-- ADD YOUR OWN FAVICON IF YOU WANT-->
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  
   <!-- END OPENGRAPH -->
 `
 }
